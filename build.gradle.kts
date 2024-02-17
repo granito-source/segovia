68d1c28c0835599fd663770e5ace19f66d4e09b1
Alexei Yashkov <alexei.yashkov@yashkov.org>
2024-02-17 15:06:42 -0500

    Story #2: update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 plugins {
     id("base")
-    kotlin("jvm") version "1.9.21" apply false
+    kotlin("jvm") version "1.9.22" apply false
     id("io.spring.dependency-management") version "1.1.4"
-    id("org.springframework.boot") version "3.2.0" apply false
+    id("org.springframework.boot") version "3.2.2" apply false
 }
 
 subprojects {
