33569cc6ef7f88d0bac86bbd17dc41e5f0d19f96
Alexei Yashkov <alexei.yashkov@yashkov.org>
2025-12-27 15:23:56 -0500

    Relax timestamp checking even further

--- web/src/test/kotlin/io/granito/segovia/web/controller/Assertions.kt
+++ web/src/test/kotlin/io/granito/segovia/web/controller/Assertions.kt
@@ -1,6 +1,7 @@
 package io.granito.segovia.web.controller
 
 import java.time.Instant
+import java.time.temporal.ChronoUnit
 import org.assertj.core.api.Assertions.assertThat
 import org.springframework.http.MediaType
 import org.springframework.test.web.reactive.server.WebTestClient
@@ -12,8 +13,8 @@ fun WebTestClient.ResponseSpec.expect5xxError(from: Instant, to: Instant):
         .jsonPath("$.status").isEqualTo(500)
         .jsonPath("$.error").isEqualTo("Internal Server Error")
         .jsonPath("$.timestamp").value<String> {
-            assertThat(Instant.parse(it))
-                .isAfterOrEqualTo(from)
-                .isBeforeOrEqualTo(to)
+            assertThat(Instant.parse(it).truncatedTo(ChronoUnit.SECONDS))
+                .isAfterOrEqualTo(from.truncatedTo(ChronoUnit.SECONDS))
+                .isBeforeOrEqualTo(to.truncatedTo(ChronoUnit.SECONDS))
         }
         .jsonPath("$.requestId").isNotEmpty
