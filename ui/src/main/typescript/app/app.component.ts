9b0bed3db94ad0fe81e12d5c39a12bb627fbe43a
Alexei Yashkov <alexei.yashkov@yashkov.org>
2025-12-27 15:51:37 -0500

    Update to Angular Material 21

--- ui/src/main/typescript/app/app.component.ts
+++ ui/src/main/typescript/app/app.component.ts
@@ -1,8 +1,8 @@
-
 import { Component, OnInit } from '@angular/core';
 import { MatIconModule, MatIconRegistry } from '@angular/material/icon';
 import { DomSanitizer, Title } from '@angular/platform-browser';
 import { RouterLink, RouterOutlet } from '@angular/router';
+import { CommonModule } from "@angular/common";
 
 @Component({
     selector: 'app-root',
